51c78c99fe8219c0785cb422375db3f316a650f8
forturnately <fortunate3308@gmail.com>
2022-09-13 21:09:52 -0700

    sovled

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
-#include <unistd.h>                                                               
-#include "main.h" 
+#include <stdio.h>
+#include <main.h>
 
 /**
  * main - program that prints _putchar.
