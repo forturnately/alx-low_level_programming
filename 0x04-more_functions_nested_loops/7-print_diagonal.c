c16c2c840d26e9fa7064d4182d68b7156a8bb15e
forturnately <fortunate3308@gmail.com>
2022-09-16 20:29:42 -0700

    solved

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,22 +8,22 @@
 
 void print_diagonal(int n)
 {
-	int i = 0, ii;
+int i = 0, ii;
 
-	while (i < n && n > 0)
+while (i < n && n > 0)
+{
+	ii = 0;
+	while (ii < i)
 	{
-		ii = 0;
-		while (ii < i)
-		{
-			_putchar(' ');
-			ii++;
-		}
-		_putchar('\\');
-		_putchar('\n');
-		i++;
+		_putchar(' ');
+		ii++;
 	}
-	if (i == 0)	
-	{
-		_putchar('\n');
-	}
+	_putchar('\\');
+	_putchar('\n');
+	i++;
+}
+if (i == 0)	
+{
+	_putchar('\n');
+}
 }
