7ee722164bb1515469d161d7191d4c7932a5d694
forturnately <fortunate3308@gmail.com>
2022-09-23 21:24:12 -0700

    solved

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -9,15 +10,15 @@
 void print_number(int n)
 {
 	unsigned int n1;
-	n1 = n;
 
+	n1 = n;
 	if (n < 0)
 	{
 		_putchar('-');
 		n1 = -n;
 	}
 	if (n1 / 10 != 0)
-	{	
+	{
 		print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
